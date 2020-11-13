605554ed23cb58be6f9cb4e339426e282619627c
Faith Magras <bdanielle092@gmail.com>
2020-11-13 10:05:23 -0600

    prompted the users

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace EnthusiasticMoose
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Magic Moose !");
+            Console.WriteLine("--------------------------------------------");
+            Console.WriteLine();
+
+            //let the moose speak!
+            MooseSays("Ask me a question...");
+        }
+
+        static void MooseSays(string message)
+        {
+            Console.WriteLine($@"
+                                       _.--^^^--,
+                                    .'          `\
+  .-^^^^^^-.                      .'              |
+ /          '.                   /            .-._/
+|             `.                |             |
+ \              \          .-._ |          _   \
+  `^^'-.         \_.-.     \   `          ( \__/
+        |             )     '=.       .,   \
+       /             (         \     /  \  /
+     /`               `\        |   /    `'
+     '..-`\        _.-. `\ _.__/   .=.
+          |  _    / \  '.-`    `-.'  /
+          \_/ |  |   './ _     _  \.'
+               '-'    | /       \ |
+                      |  .-. .-.  |            {message}
+                      \ / o| |o \ /
+                       |   / \   |   
+                      / `^`   `^` \
+                     /             \
+                    | '._.'         \
+                    |  /             |
+                     \ |             |
+                      ||    _    _   /
+                      /|\  (_\  /_) /
+                      \ \'._  ` '_.'
+                       `^^` `^^^`
+            ");
+        }
+    }
+}
